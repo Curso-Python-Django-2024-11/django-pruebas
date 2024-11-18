a327479d4e8ccef0b9c3a759aa8dedffa037523a
Jairo Chapela-Martínez <jairochapela@gmail.com>
2024-11-18 15:45:36 +0100

    First commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/postlist.html'

68c9d423f3104345ae195e522ebf5d7eee73e706
Jairo Chapela-Martínez <jairochapela@gmail.com>
2024-11-20 12:21:46 +0100

    Detalle de post y jerarquía de plantillas

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from blog.views import PostListView
+from blog.views import PostDetailView, PostListView
 from django.urls import path
 
 
 urlpatterns = [
    path('', PostListView.as_view(), name='post_list'),
+   path('posts/<slug:slug>', PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 # Create your views here.
+
+
+# Lista de posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/postlist.html'
+
+
+# Detalle de un post concreto
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/postdetail.html'
